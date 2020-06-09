45345ee62f3d4468422875ef6e82ece2de8a2fe9
Andrew Yates <andrew@laneyates.com>
2020-06-09 15:04:01 -0400

    sensible numbers for /, -

--- arithmetic.js
+++ arithmetic.js
@@ -1,11 +1,12 @@
 //TODO:
 //DONE - operation
 //DONE - number sizes
-//check answer correctness
-//make numbers make sense for given operation
-//put largest number on top, same as above
+//DONE - check answer correctness
+//DONE - make numbers make sense for given operation
+//DONE - put largest number on top, same as above
 //carrying or not carrying
 //move answer line up a bit?
+//make division problems more random
 
 var cop = "+";
 var operations = ["add", "sub", "mult", "div"];
@@ -21,7 +22,7 @@ for(var i = 0; i < operations.length; i++){
 var max_in = document.getElementById("max");
 var max;
 max_in.oninput = function(){
-  max = max_in.value;
+  max = parseInt(max_in.value)+1;
   new_problem();
 }
 
@@ -51,11 +52,32 @@ function answer(operation, num1, num2){
   }
 }
 
+function assign_nums(){
+  n1 = Math.floor(Math.random() * max);
+  n2 = Math.floor(Math.random() * max);
+  if(cop == "-"){
+    n2 = Math.floor(Math.random() * n1);
+  }
+  if(cop == "/"){
+    n2 = Math.max(Math.floor(Math.random() * Math.sqrt(n1))+1, 1);
+    //console.log(n1, n2);
+    n1 = Math.max(Math.floor(n1/n2) * n2, 1);
+    if(Math.random() < .5){
+      n2 = n1/n2;
+    }
+    //console.log(n1);
+  }
+  return [n1, n2];
+}
+
 function new_problem(){
   op_display.textContent = cop;
   //console.log(op_display.getBoundingClientRect().width);
-  n1 = Math.floor(Math.random()*max);
-  n2 = Math.floor(Math.random()*max);
+  //n1 = Math.floor(Math.random()*max);
+  //n2 = Math.floor(Math.random()*max);
+  nums = assign_nums();
+  n1 = nums[0];
+  n2 = nums[1];
   a = answer(cop, n1, n2);
   n1e.textContent = n1;
   n2e.textContent = n2;
